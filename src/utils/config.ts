f914a91358b862aad02ba10162466cded2f85f3a
jcarrillo <130939221+JudaCarrillo@users.noreply.github.com>
2024-01-18 21:20:23 -0500

    Add header

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -116,7 +116,7 @@ const getMetadata = () => {
 
 const getI18N = () => {
   const _default = {
-    language: 'en',
+    language: 'es',
     textDirection: 'ltr',
   };
 
--- src/utils/permalinks.ts
+++ src/utils/permalinks.ts
@@ -30,6 +30,7 @@ export const POST_PERMALINK_PATTERN = trimSlash(APP_BLOG?.post?.permalink || `${
 /** */
 export const getCanonical = (path = ''): string | URL => {
   const url = String(new URL(path, SITE.site));
+  console.log(path)
   if (SITE.trailingSlash == false && path && url.endsWith('/')) {
     return url.slice(0, -1);
   } else if (SITE.trailingSlash == true && path && !url.endsWith('/')) {
